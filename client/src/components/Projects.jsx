704be7d9466cf8f7ecef3d461a6286850f34a6ea
Vu Huynh <vqhuynh3@gmail.com>
2020-11-02 22:47:21 -0800

    refactor styling

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -5,6 +5,23 @@ class Projects extends Component {
     return (
       <div className="project-container">
         <h1 className="project-title">Development Projects</h1>
+        <div className="tabs">
+        <h1>React Tabs</h1>
+       <Tabs>
+         <Tab label="Tab 1">
+           <div>
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0xIAJciTxzjZZM-viJfHk_Ezlv0nn8fft3ORQ9UkoP2mPd5pm"/>
+             <p>Tab 1 content</p>
+           </div>
+         </Tab>
+         <Tab label="Tab 2">
+           <div>
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2E3SrDqYWF9J_TDWNzjZkwJOAGxkLJVZPvUly7SqUL9WqLqEw"/>
+             <p>Tab 2 content</p>
+           </div>
+         </Tab>
+        </Tabs>
+         </div>
       </div>
     );
   }
